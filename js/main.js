eefcfd616f3b6af5f1eadcebf88b7359c9741e28
Riaburger <jacopo.riati@edu.unifi.it>
2024-04-30 20:24:11 +0200

    Almost finished

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 function blurBackground() {
-    $('main').not('collapse').css("filter","blur(2px)");
+    $('main').not('collapse').css("filter","blur(3px)");
+}
+
+function closeNavbar() {
+    $('#navbarToggler').collapse('hide');
+
+    setTimeout(function() {
+        $('main').css("filter","none");
+    }, 300); // 500 milliseconds = 0.5 seconds
 }

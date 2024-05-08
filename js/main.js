b98a57d6ee239c1eeab2083b588cc7f44d2e82b4
Riaburger <jacopo.riati@edu.unifi.it>
2024-05-08 18:33:43 +0200

    thumbnail video system added

--- js/main.js
+++ js/main.js
@@ -9,3 +9,24 @@ function closeNavbar() {
         $('main').css("filter","none");
     }, 300); // 500 milliseconds = 0.5 seconds
 }
+$(document).ready(function() {
+    var currentRow = 1;
+    var totalRows = 3; // Update this to match the number of rows
+
+    $('.showMoreThumb').click(function() {
+        // Hide the current row
+
+        // Show the next row
+        currentRow++;
+        if (currentRow <= totalRows) {
+            $('#row' + currentRow).collapse('show');
+        }
+        if (currentRow > totalRows) {
+            $(this).prop('disabled', true);
+        } else {
+            // If there are more rows, remove the button
+            $(this).remove();
+        }
+    });
+});
+
